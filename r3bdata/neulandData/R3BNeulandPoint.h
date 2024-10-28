e846d9733353b402179e3994e1a969c2d758bd55
Simon Lonzen <slonzen@theia.ikp.uni-koeln.de>
2024-10-28 16:15:05 +0100

    clean changes

--- r3bdata/neulandData/R3BNeulandPoint.h
+++ r3bdata/neulandData/R3BNeulandPoint.h
@@ -65,9 +65,9 @@ class R3BNeulandPoint : public FairMCPoint
     [[nodiscard]] auto GetPaddle() const -> Int_t { return GetDetectorID(); }
     [[nodiscard]] auto GetLightYield() const -> Double_t { return fLightYield; }
     [[nodiscard]] auto GetPID() const -> int { return particle_id_; }
-    ClassDefOverride(R3BNeulandPoint, 1)
+    ClassDefOverride(R3BNeulandPoint, 2);
 
-        void Print(const Option_t* /*opt*/) const override;
+    void Print(const Option_t* /*opt*/) const override;
 
   protected:
     Double_t fLightYield;
