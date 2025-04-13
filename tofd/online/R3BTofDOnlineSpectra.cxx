525521d78be4f0eef67d3d97d14301d7f0bc7491
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2025-04-13 20:14:01 +0200

    Test

--- tofd/online/R3BTofDOnlineSpectra.cxx
+++ tofd/online/R3BTofDOnlineSpectra.cxx
@@ -717,7 +717,7 @@ void R3BTofDOnlineSpectra::Reset_Histo()
         {
             fh_tofd_dt_hit[i]->Reset();
         }
-        fh2_tofd_time_vs_charge->Reset();fh2_tofd_time_los_cal
+        fh2_tofd_time_vs_charge->Reset();
         fh2_tofd_charges13->Reset();
         fh2_tofd_charges24->Reset();
         fh2_tofd_charges12->Reset();
